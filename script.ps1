f9d2e3fd3d03cdebfea9d3c0a6e109fca5349530
benmen1980 <benmen1@gmail.com>
2018-09-06 02:32:07 +0300

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,6 +1,7 @@
-﻿
+# 10619 "SU (Aeroflot)" SH14000037 SU13812 Jerusalem "47 Navi St. Jerusalem" 050654987321 050987654321 05065487987 Y
+
 #if logs directory does not exist, create it
-if( -Not (Test-Path .\logs\) ){
+if( -Not (Test-Path C:\priority\bin.95\spix_api\logs\) ){
     mkdir logs
 }
 
@@ -8,8 +9,8 @@ if( -Not (Test-Path .\logs\) ){
 $date = date -Format u
 $date = ($date.Substring(0,10)).Replace('-','')
 $date = $date.Replace('-','')
-$errorLog = '.\logs\'+$date+'-Error.log'
-$runLog = '.\logs\'+$date+'-Run.log'
+$errorLog = 'C:\priority\bin.95\spix_api\logs\'+$date+'-Error.log'
+$runLog = 'C:\priority\bin.95\spix_api\logs\'+$date+'-Run.log'
 
 #this function logs any errors and stops the script
 function errorLog{
@@ -34,9 +35,9 @@ function log{
 #this function reads the config file and stores entries in global variables
 function readConfigFile{
     #test if config file exists; otherwise write to log
-    if(Test-Path config.cfg){
+    if(Test-Path C:\priority\bin.95\spix_api\config.cfg){
         #if config file exists, check if all needed entries are supplied
-        $configFileContents = Get-Content config.cfg
+        $configFileContents = Get-Content C:\priority\bin.95\spix_api\config.cfg
         #check if all config file entries are supplied
         if($configFileContents.Length -ne 7){
             errorLog 'Config file incomplete.'
@@ -65,9 +66,11 @@ function readConfigFile{
 function createRequest{
     $secpassword = ConvertTo-SecureString $password -AsPlainText -Force
     $credential = New-Object System.Management.Automation.PSCredential($user, $secpassword)
-    try{        
-        $webRequest = Invoke-WebRequest -Uri $serviceUrl -Credential $credential -Body $args[0] -Method POST
-        $statusCode = [int]$webRequest.StatusCode
+    try{
+        $webRequest = Invoke-WebRequest -Uri $serviceUrl -Credential $credential -Method POST -Body $args[0]   #([System.Text.Encoding]::UTF8.GetBytes($args[0])) 
+		#$webRequest = Invoke-RestMethod -Method Post -Uri $serviceUrl -Credential $credential -Body ( $args[0]) #-Header @{"X-ApiKey"=$apiKey}   ConvertTo-Json
+        $statusCode = [int]$webRequest.StatusCode 
+		log $webRequest
         if($statusCode -eq 200){
             $global:responseFlag=0
         }
@@ -93,10 +96,10 @@ function databaseUpdate{
     $Command = New-Object System.Data.SQLClient.SQLCommand
     $Command.Connection = $connection
     if($responseFlag -eq 0){
-        $SQLQuery = "UPDATE DOCUMENTS SET DETAILS=`'OK`' WHERE DOC="+$doc
+        $SQLQuery = "UPDATE DOCUMENTS SET ELMG_SPIX=5 WHERE DOC="+$doc
     }
     else{
-        $SQLQuery = "UPDATE DOCUMENTS SET DETAILS=`'ERR`' WHERE DOC="+$doc
+        $SQLQuery = "UPDATE DOCUMENTS SET ELMG_SPIX=6 WHERE DOC="+$doc
     }
     $Command.CommandText = $SQLQuery
 
@@ -122,23 +125,24 @@ readConfigFile
 log 'Config file parsed. Converting arguments to JSON.'
 
 #create JSON out of args array for POST
-$params = [PsCustomObject]@{
+#$params = [PsCustomObject]@{
+$params = @{
     DOC=''+$args[0]
     CDES=''+$args[1]
     DOCNO=''+$args[2]
     BOOKNUM=''+$args[3]
     ELIT_CITYNAME=''+$args[4]
     ELIT_FULLADDRESS=''+$args[5]
-    ELYD_CELL1=''+$args[6]
-    ELYD_CELL2=''+$args[7]
-    ELYD_CELL=''+$args[8]
+    ELYD_CELL=''+$args[6]
+    #ELYD_CELL2=''+$args[7]
+    #ELYD_CELL=''+$args[8]
     IS_ENGLISH=''+$args[9]
 }
 $global:doc = ''+$args[0]
-$postParams = $params | ConvertTo-JSON
+$postParams = $params #| ConvertTo-JSON
 
 log 'POST JSON created. Creating request.'
-
+log $postParams
 #call to the function that does the POST to the webservice with JSON data as param and store the response
 createRequest $postParams
 
@@ -153,3 +157,4 @@ log 'Connection defined. Running update statement.'
 databaseUpdate
 
 log 'Update done. Work is done.'
+
